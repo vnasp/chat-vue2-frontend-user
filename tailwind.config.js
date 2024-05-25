1fdc7f8e443fc5355017c0b77dc76d3c08cb10ee
Valentina Muñoz <valentinamr@gmail.com>
2024-05-25 17:15:40 -0400

    Update deploy settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  content: [],
+  content: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
   theme: {
     extend: {
       colors: {
         chat: "#f0f2f5",
         whatsapp: {
           950: "#00a884",
-          500: "#e0fcd7"
+          500: "#e0fcd7",
+        },
       },
     },
   },
-},
   plugins: [],
 };
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: "/chat-vue2-frontend-user/",
   transpileDependencies: true
 })
