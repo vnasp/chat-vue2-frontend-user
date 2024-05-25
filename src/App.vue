3d36de6144b619c058b618bedfd680b43f175335
Valentina Muñoz <valentinamr@gmail.com>
2024-05-25 13:11:46 -0400

    Update styles

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,61 @@
 <template>
-  <div id="app" class="w-screen h-screen p-5">
-    <div class="flex gap-4 w-full h-full">
-      <div class="rounded-md flex flex-col bg-chat w-1/4 p-4">
-        <img src=""/>
-        <h2>nombre usuario</h2>
-        <select>
-          <option>colores</option>
-        </select>
-        <textarea>blabla</textarea>
-        <button>enviar</button>
+  <div id="app" class="w-screen h-screen p-6">
+    <div class="flex gap-6 h-full">
+      <!-- Perfil Usuario 1 -->
+      <div class="user">
+        <div class="flex flex-col items-center">
+          <img src="./assets/img/user.png" class="user__avatar" />
+          <h2 class="user__name">nombre usuario</h2>
+        </div>
+        <div class="flex flex-col w-full">
+          <label htmlFor="messageColor" class="text-start">Color mensaje</label>
+          <select id="messageColor" class="h-10">
+            <option>colores</option>
+          </select>
+        </div>
+        <div class="flex flex-col w-full">
+          <label htmlFor="messageText" class="text-start">Mensaje</label>
+          <textarea id="messageText" class="w-full p-4" rows="6">blabla</textarea>
+        </div>
+        <button class="btn btn--primary">enviar</button>
       </div>
-      <div id="chat" class="rounded-md w-1/2 p-4">chat</div>
-      <div class="rounded-md bg-chat w-1/4 p-4">persona 2</div>
+
+      <div id="chat" class="rounded-md w-1/2 p-6 flex flex-col">
+        un array de nombre, mensaje y color. si es usuario 1 es text-left, si es usuario 2 es text-right
+        <!-- Mensaje del usuario 1 -->
+        <div class="message__box--user1 bg-white">
+          <h4 class="message__user">Nombre Usuario 1</h4>
+          <p class="message__text--user1">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci atque sapiente iusto in, obcaecati est quam tempora reiciendis sequi explicabo corporis dolores incidunt eius alias officia? Laborum suscipit quas voluptatum?</p>
+        </div>
+
+        <!-- Mensaje del usuario 2 -->
+        <div class="message__box--user2 bg-whatsapp-500">
+          <h4 class="message__user">Nombre Usuario 2</h4>
+          <p class="message__text--user2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi accusamus, labore in magni voluptates esse? Voluptatem quaerat nobis fugit nesciunt. Possimus deserunt laborum quas facere sint eius cupiditate, temporibus adipisci.</p>
+        </div>
+      </div>
+
+      <!-- Perfil Usuario 2 -->
+      <div class="user">
+        <div class="flex flex-col items-center">
+          <img src="./assets/img/user.png" class="user__avatar" />
+          <h2 class="user__name">nombre usuario</h2>
+        </div>
+        <div class="flex flex-col w-full">
+          <label htmlFor="messageColor" class="text-start">Color mensaje</label>
+          <select id="messageColor" class="h-10">
+            <option>colores</option>
+          </select>
+        </div>
+        <div class="flex flex-col w-full">
+          <label htmlFor="messageText" class="text-start">Mensaje</label>
+          <textarea id="messageText" class="w-full p-4" rows="6">blabla</textarea>
+        </div>
+        <button class="btn btn--primary">enviar</button>
+      </div>
     </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    </div>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
 </template>
 
 <script>
@@ -28,5 +69,4 @@ export default {
 }
 </script>
 
-<style>
-</style>
+<style></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,15 @@ module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   content: [],
   theme: {
-    extend: {},
-    colors: {
-      chat: "#f0f2f5",
+    extend: {
+      colors: {
+        chat: "#f0f2f5",
+        whatsapp: {
+          950: "#00a884",
+          500: "#e0fcd7"
+      },
     },
   },
+},
   plugins: [],
 };
