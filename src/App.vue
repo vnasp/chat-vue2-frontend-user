2374143131f2c4d921facb7456fb213bd280221f
Valentina Muñoz <valentinamr@gmail.com>
2024-05-25 11:31:25 -0400

    Add project and base styles

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app" class="w-screen h-screen p-5">
+    <div class="flex gap-4 w-full h-full">
+      <div class="rounded-md flex flex-col bg-chat w-1/4 p-4">
+        <img src=""/>
+        <h2>nombre usuario</h2>
+        <select>
+          <option>colores</option>
+        </select>
+        <textarea>blabla</textarea>
+        <button>enviar</button>
+      </div>
+      <div id="chat" class="rounded-md w-1/2 p-4">chat</div>
+      <div class="rounded-md bg-chat w-1/4 p-4">persona 2</div>
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: [],
+  theme: {
+    extend: {},
+    colors: {
+      chat: "#f0f2f5",
+    },
+  },
+  plugins: [],
+};
